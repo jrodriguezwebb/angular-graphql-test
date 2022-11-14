dfcff77c740145d573c8a2053f509505c52282a8
jarodriguezp <jarodriguezp@indra.es>
2022-11-14 19:34:15 +0100

    Adding improvements

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CoreModule, SharedModule, AppRoutingModule, HttpClientModule],
+  imports: [CoreModule, SharedModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
