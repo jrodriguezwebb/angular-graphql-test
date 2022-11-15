b4724c600b2294d34f74e5cd71e01cb8ebb91b23
jarodriguezp <jarodriguezp@indra.es>
2022-11-15 15:08:54 +0100

    Adding better async handling

--- app/src/app/boxes/selected-box/selected-box.component.ts
+++ app/src/app/boxes/selected-box/selected-box.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { Edge } from 'src/app/shared/interfaces/edge.interface';
 import { BoxService } from 'src/app/shared/services/box.service';
 import { Router } from '@angular/router';
 import { BoxesRoutesEnum } from '../boxes.routes';
 import { OpenBoxClass } from 'src/app/shared/interfaces/open-box.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-selected-box',
@@ -15,7 +16,7 @@ export class SelectedBoxComponent {
   public selectedBox: Edge | undefined;
   public isLoading = false;
   public opened = false;
-  public openBox: OpenBoxClass | undefined;
+  public openBox$: Observable<OpenBoxClass | undefined>;
   constructor(private readonly boxService: BoxService, private router: Router) {
     this.selectedBox = this.boxService.getSelectedBox();
     if (!this.selectedBox) {
@@ -33,18 +34,15 @@ export class SelectedBoxComponent {
         amount: Math.ceil(cost),
         multiplierBoxBet: 1.0,
       };
-      this.boxService
-        .openBox(openBoxInput)
-        .pipe(
-          catchError(error => {
-            this.isLoading = false;
-            throw new Error(error);
-          })
-        )
-        .subscribe(openBox => {
-          this.openBox = openBox;
+      this.openBox$ = this.boxService.openBox(openBoxInput).pipe(
+        tap(() => {
           this.isLoading = false;
-        });
+        }),
+        catchError(error => {
+          this.isLoading = false;
+          throw new Error(error);
+        })
+      );
     }
   }
 }
